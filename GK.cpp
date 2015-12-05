f0191a8333b330b4972116fc5df7be61b5b28195
Piotr Makowiec <pieterm4@wp.pl>
2015-12-05 18:46:54 +0100

    Update GK.cpp

--- GK.cpp
+++ GK.cpp
@@ -2,7 +2,7 @@
 
 GK::GK(FileReader& _file)  : Format(_file)
 {
-	// TODO Auto-generated constructor stub
+	buffer = _file.GetContent();
 
 }
 
@@ -13,5 +13,30 @@ GK::~GK()
 
 bool GK::isValid()
 {
-	return true;
+	uint8_t temp1 = *buffer;
+	uint8_t temp2 = *buffer + 1;
+	uint8_t temp3;
+	uint8_t temp4;
+
+	uint16_t HeaderGK = (temp2 << 8) | temp1;
+
+	temp1 = *buffer + 14;
+	temp2 = *buffer + 15;
+
+	uint16_t HeaderBMP = (temp2 << 8) | temp1;
+
+	temp1 = *buffer + 55;
+	temp2 = *buffer + 56;
+	temp3 = *buffer + 57;
+	temp4 = *buffer + 58;
+	uint16_t HeaderSizeBMP1 = (temp2 << 8) | temp1;
+	uint16_t HeaderSizeBMP2 = (temp4 << 8) | temp1;
+
+	uint32_t HeaderSizeBMP = (HeaderSizeBMP2 << 16) | HeaderSizeBMP1;
+	if (HeaderGK == HeadNameGK && HeaderBMP == HeadNameBM && HeaderSizeBMP == HeadSizeBMP)
+	{
+		return true;
+	}
+	else
+		return false;
 }
