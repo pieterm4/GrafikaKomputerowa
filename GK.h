cf747319499c3c9295b007c46489dd9e649eaaa0
Piotr Makowiec <pieterm4@wp.pl>
2015-12-06 01:06:08 +0100

    Update GK.h

--- GK.h
+++ GK.h
@@ -6,22 +6,60 @@
 class GK : public Format
 {
 	private:
-	
-	//GK Header
-	uint16_t HeadNameGK = 0x474B;
+	////GK Header
+	//uint16_t HeadNameGK = 0x474B;
 
 
-	//BitmapHeader
-	uint16_t HeadNameBM = 0x424D;
+	////BitmapHeader
+	//uint16_t HeadNameBM = 0x424D;
 
 
-	//BitmapInfoheader
-	uint32_t HeadSizeBMP = 0x28;
+	////BitmapInfoheader
+	//uint32_t HeadSizeBMP = 0x28;
 
-	uint16_t BytesPerPixel = 0x4;
+	//uint16_t BytesPerPixel = 0x4;
 
-	//Input buffer
+
+	////Input buffer
 	uint8_t *buffer;
+
+	typedef struct GKFILEHEADER
+	{
+		short type;
+		int fileSize;
+		int width;
+		int height;
+	} GKFILEHEADER;
+
+	/* File Bitmap Header */
+	typedef struct BMPFILEHEADER {
+		short type;
+		int fileSize;
+		short reserved0;
+		short reserved1;
+		int dataOffset;
+	} BMPFILEHEADER;
+
+	/* Info Header */
+	typedef struct BMPINFOHEADER {
+		int hdrSize;
+		int width;
+		int height;
+		short planes;
+		short depth;
+		int compression;
+		int bmpDataSize;
+		int hResolution;
+		int vResolution;
+		int nColors;
+		int nImportantColors;
+	} BMPINFOHEADER;
+
+	typedef struct RGB {
+		unsigned char R;
+		unsigned char G;
+		unsigned char B;
+	} Pixel24;
 public:
 	GK(FileReader& _file);
 	virtual ~GK();
